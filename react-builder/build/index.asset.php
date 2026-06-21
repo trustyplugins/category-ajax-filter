713f29f49334cfce5fb98dbcfd0d4ad54f07881b
trustyplugins <trustyplugins@gmail.com>
2026-06-21 21:47:46 +0530

    Update - gradient

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'ab8440fd5388c3b07438');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '4ad26caaff8a3b1f7303');
