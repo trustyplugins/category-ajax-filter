6b73908eda0f1826f58e0961f92927b2d51eb655
trustyplugins <trustyplugins@gmail.com>
2026-06-22 17:25:31 +0530

    Update - 3.0 build

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'cf10d1a2a65b769cd80b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'eb1fafbbb83b5b86a647');
