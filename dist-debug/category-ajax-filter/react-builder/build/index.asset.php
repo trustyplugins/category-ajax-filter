6a0ccfe8125b2bc264f9b3bde6501a9c61cad033
trustyplugins <trustyplugins@gmail.com>
2026-08-04 15:40:45 +0530

    Update - Show more button issue

--- dist-debug/category-ajax-filter/react-builder/build/index.asset.php
+++ dist-debug/category-ajax-filter/react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '929539a3215596936514');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '4fe80c3e82f285a966b5');
