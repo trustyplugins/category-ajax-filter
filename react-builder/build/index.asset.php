e7b3d44293ff9d4e6e01be7251126b321ff51152
trustyplugins <trustyplugins@gmail.com>
2026-08-04 17:28:09 +0530

    Update - any selection fix

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'fbf65d8a3aec81ee6006');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '8d6b463ff7524c5e42c4');
