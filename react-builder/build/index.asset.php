78758954047dda00ca38d64118fa10391edcbe25
trustyplugins <trustyplugins@gmail.com>
2026-08-04 17:51:07 +0530

    update - color auto refresh

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '8d6b463ff7524c5e42c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '752d35668c6482850bae');
