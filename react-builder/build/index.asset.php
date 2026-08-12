febd38a0358d921a548301edfc547fd0c52b5b32
trustyplugins <trustyplugins@gmail.com>
2026-08-12 15:26:53 +0530

    Update - search fix

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '5cca59c4ade0832b7795');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '3bff85f3baa035f8b493');
