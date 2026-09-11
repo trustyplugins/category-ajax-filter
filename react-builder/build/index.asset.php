7ff5b121373cdf7ef8b09447047ae3adcf60db82
SachinKhajuria007 <sachin@trustycoders.com>
2026-09-11 12:57:32 +0530

    Category / Term Page Mode move to filter settings

--- react-builder/build/index.asset.php
+++ react-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'bdcd7e232b62acabb5b5');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '2285ea278fe97ae093ed');
